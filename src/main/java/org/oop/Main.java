d7aec08212c37bf92c020e0ab0da677ca73570cf
Antonio Tomaquin <antoniotomaquin@gmail.com>
2025-10-28 15:05:55 +0800

    oop reviewer

--- src/main/java/org/oop/Main.java
+++ src/main/java/org/oop/Main.java
@@ -1,5 +1,11 @@
 package org.oop;
 
+
+import org.oop.encapsulation.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,5 +15,17 @@ public class Main {
         myCar.startEngine();
         myCar.accelerate();
         myCar.brake();
+
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("John Doe");
+        employee.setUsername("johndoe");
+        employee.setPassword("password123");
+
+        System.out.println("Employee Details:");
+        System.out.println("ID: " + employee.getId());
+        System.out.println("Name: " + employee.getName());
+        System.out.println("Username: " + employee.getUsername());
+        // Note: In a real application, avoid printing passwords
     }
 }
