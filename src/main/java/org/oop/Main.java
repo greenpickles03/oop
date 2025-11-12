7c41c0b920f80a0b03f2e34eace145fc0eaf093c
greenpickles <greenpickles03@github.com>
2025-11-12 17:14:07 +0800

    update

--- src/main/java/org/oop/Main.java
+++ src/main/java/org/oop/Main.java
@@ -51,5 +51,14 @@ public class Main {
 
         Immutable obj = new Immutable( "Immutable Object");
         System.out.println("Immutable Object ID: " + obj.getName());
+
+        String a = new String("Hello");
+        String b = new String("Hello");
+        String c = "Hello";
+        String d = "Hello";
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        System.out.println(a == c);
+        System.out.println(c == d);
     }
 }
